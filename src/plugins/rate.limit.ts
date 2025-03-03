83e9cb0a2f9064a915491e118f4da9d854920164
Nizam Chaudhary <ChaudharyNizam10@gmail.com>
2025-03-03 21:19:50 +0530

    implement fastify rate limitter

---
+++ src/plugins/rate.limit.ts
@@ -0,0 +1,10 @@
+import fastifyRateLimit from "@fastify/rate-limit";
+import fastifyPlugin from "fastify-plugin";
+
+export default fastifyPlugin((fastify, opts, done) => {
+  fastify.register(fastifyRateLimit, {
+    max: 100,
+    timeWindow: "1 minute",
+  });
+  done();
+});
